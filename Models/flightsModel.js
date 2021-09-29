eeac4934908e6d0dae65cf8034f00c9403bf1489
HemaShyamala <hemashyamala.naidu@gmail.com>
2021-09-29 20:15:43 +0530

    Airport Data

--- Models/flightsModel.js
+++ Models/flightsModel.js
@@ -1,34 +1,35 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const FlightSchema = mongoose.Schema({
-    logo:{
-        type:String,
-        
+const FlightSchema = mongoose.Schema(
+  {
+    logo: {
+      type: String,
     },
-    title:{
-        type:String,
-        require:true,
+    title: {
+      type: String,
+      require: true,
     },
-    time1:{
-        type:String,
-        require:true,
+    time1: {
+      type: String,
+      require: true,
     },
-    time2:{
-        type:String,
-        require:true,
+    time2: {
+      type: String,
+      require: true,
     },
-    stops:{
-        type:String,
-        default:"non stop",
+    stops: {
+      type: String,
+      default: "non stop",
     },
-    price:{
-        type:Number,
-        default:7452,
-        require:true
-    }
-},
-{
-    versionKey:false
-});
+    price: {
+      type: Number,
+      default: 7452,
+      require: true,
+    },
+  },
+  {
+    versionKey: false,
+  }
+);
 
-module.exports = mongoose.model('Flight',FlightSchema);
+module.exports = mongoose.model("Flight", FlightSchema);
